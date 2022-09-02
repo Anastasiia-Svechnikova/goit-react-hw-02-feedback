10d2bdaed5bb2cb354c0d3750908f0748319f877
Anastasiia-Svechnikova <anastasiiakundriukova@ymail.com>
2022-09-03 01:27:08 +0300

    task 1 completed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,40 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from "react";
+import Section from "./section";
+import Statistics from "./statistics";
+import FeedbackOptions from "./feedback-options";
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral:0,
+    bad: 0,
+  }
+  onLeaveFeedback = (opt) => {
+    this.setState((prevState => {
+      return {
+        [opt]: prevState[opt] +1
+      }
+    }))
+  
+  }
+
+  
+  render() {
+    const { good, bad, neutral } = this.state;
+    const total = Object.values(this.state).reduce((acc, el) => acc += el, 0);
+    const positivePercentage = (good * 100) / total;
+    
+    return (
+      <div className = "app" >
+      <Section title='Please Leave Feedback'>
+          <FeedbackOptions options={['good', 'bad', 'neutral']} onLeavefeedback={ this.onLeaveFeedback} />
+        </Section>
+        {total !== 0 &&  <Section title='Statistics'>
+          <Statistics good={good} bad={bad} neutral={neutral} total={total} positivePercentage ={positivePercentage} />
+      </Section>}
+     
+    </div >
+    )
+  }
 };
+
